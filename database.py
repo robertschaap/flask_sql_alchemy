090209802ccb27d9fe681094e85cb1465cc37748
robertschaap <robert.schaap@mac.com>
2018-11-28 22:05:44 +0100

    database: setup basic database

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+db_string = "postgres://localhost:5432/test"
+engine = create_engine(db_string, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
