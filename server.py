ee726198a480b6a458463dc1385465fec8b155f2
robertschaap <robert.schaap@mac.com>
2018-11-28 22:07:16 +0100

    server: import and teardown database connection

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from database import init_db, db_session
+
 app = Flask(__name__)
+init_db()
 
 @app.route("/")
 def getIndex():
   return "Hello World"
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+  db_session.remove()
